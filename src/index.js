7807029cc0d15604505f2c5c62d757a6eebcd17f
Tetoldini Andrea <56386916+tetochannel@users.noreply.github.com>
2021-03-22 11:08:35 +0100

    Formatted code of js src files

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
 import firebase from 'firebase/app'
@@ -14,45 +14,42 @@ import 'firebase/auth'
         ]
       });*/
 
-      firebase.initializeApp({
-        apiKey: "AIzaSyDC9Fkp8M34kZ3leuhZjCV9tHxlB7upyNk",
-        authDomain: "drophereclouding.firebaseapp.com",
-        databaseURL: "https://drophereclouding-default-rtdb.firebaseio.com",
-        projectId: "drophereclouding",
-        storageBucket: "drophereclouding.appspot.com",
-        messagingSenderId: "880326133221",
-        appId: "1:880326133221:web:7db578d4e4366319be9b0c"
-    });
+firebase.initializeApp({
+  apiKey: "AIzaSyDC9Fkp8M34kZ3leuhZjCV9tHxlB7upyNk",
+  authDomain: "drophereclouding.firebaseapp.com",
+  databaseURL: "https://drophereclouding-default-rtdb.firebaseio.com",
+  projectId: "drophereclouding",
+  storageBucket: "drophereclouding.appspot.com",
+  messagingSenderId: "880326133221",
+  appId: "1:880326133221:web:7db578d4e4366319be9b0c"
+});
 
 
-      const email = "hayijow322@grokleft.com";
-      const password = "hunter2";
+const email = "hayijow322@grokleft.com";
+const password = "hunter2";
 
-function Auth()
-{
+function Auth() {
 
   const [app, setApp] = useState(null)
 
-  async function logIn()
-  {
+  async function logIn() {
     const userCredential = await firebase.auth().signInWithEmailAndPassword(email, password)
-    setApp((<App user={userCredential.user} firebase={firebase}/>));  
+    setApp((<App user={userCredential.user} firebase={firebase} />));
   }
 
-  async function signUp()
-  {
+  async function signUp() {
     const userCredential = await firebase.auth().createUserWithEmailAndPassword(email, password)
-    setApp((<App user={userCredential.user} firebase={firebase}/>));  
+    setApp((<App user={userCredential.user} firebase={firebase} />));
   }
   return (
     <div>
       {app === null ? (
-        <> 
+        <>
           <button onClick={logIn}>Accedi</button>
           <button onClick={signUp}>Registrati</button>
         </>
       ) : app}
-      
+
     </div>
   );
 }
